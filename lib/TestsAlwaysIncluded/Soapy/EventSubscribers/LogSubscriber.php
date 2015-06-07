1b59a863261b95f0ccb334f3ce89d0e9f0c18e89
Will Jaspers <wjaspers4@gmail.com>
2015-06-06 21:11:11 -0500

    Create a LogSubscriber.
    - Log requests
    - Log responses
    - Log SoapFaults
    
    Special thanks to Chris McMacken for the PHPTools library for inspiring this part!
    See: https://github.com/chrismcmacken/phptools

---
+++ lib/TestsAlwaysIncluded/Soapy/EventSubscribers/LogSubscriber.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace TestsAlwaysIncluded\Soapy\EventSubscribers;
+
+use Psr\Logger;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use TestsAlwaysIncluded\Soapy\SoapEvent;
+
+/**
+ * Logs SOAP requests and responses.
+ * Inspired by the PHPTools LoggingSoapClient created by Chris McMacken & Team
+ * @link https://github.com/chrismcmacken/phptools/tree/master/loggingsoapclient
+ */
+class LogSubscriber implements EventSubscriberInterface
+{
+	/**
+	 * Holds the logger.
+	 * @var string|false
+	 */
+	protected $logger;
+
+	/**
+	 * @param Logger $logger 
+	 */
+	public function __construct(Logger $logger)
+	{
+		$this->logger = $logger;
+	}
+
+	/**
+	 * @inheritdoc
+	 * @return  array
+	 */
+	public static function getSubscribedEvents()
+	{
+		return array(
+			array('onBeforeRequest', 0),
+			array('onResponse', 0),
+			array('onSoapFault', 0),
+		);
+	}
+
+	/**
+	 * Sets the logger.
+	 * @param Logger $logger
+	 */
+	public function setLogger(Logger $logger)
+	{
+		$this->logger = $logger;
+	}
+
+	/**
+	 * @return Logger
+	 */
+	public function getLogger()
+	{
+		return $this->logger;
+	}
+
+	/**
+	 * Logs the request.
+	 * @param SoapEvent $event
+	 */
+	public function onBeforeRequest(SoapEvent $event)
+	{
+		$message = '[SOAP] Request:%s';
+		$client = $event->getClient();
+		$context = $client->getRequestContext();
+		$action = $context->action ?: $client->__getWsdl();
+		$request = $event->getRequest();
+		$parameters = $context->all();
+		$this->logger->info(sprintf($message, $action), compact('request', 'parameters'));
+	}
+
+	/**
+	 * Logs the response.
+	 * @param SoapEvent $event
+	 */
+	public function onResponse(SoapEvent $event)
+	{
+		$message = '[SOAP] Response:%s';
+		$client = $event->getClient();
+		$context = $client->getRequestContext();
+		$action = $context->action ?: $client->__getWsdl();
+		$response = $event->getResponse();
+		$parameters = $context->all();
+		$this->logger->info(sprintf($message, $action), compact('response', 'parameters'));
+	}
+
+	/**
+	 * Logs the SoapFault
+	 * @param SoapEvent $event
+	 */
+	public function onSoapFault(SoapEvent $event)
+	{
+		$message = '[SOAP] Fault:%s';
+		$client = $event->getClient();
+		$context = $client->getRequestContext();
+		$action = $context->action ?: $client->__getWsdl();
+		$fault = $event->getFault();
+		$this->logger->info(sprintf($message, $action), compact('fault'));
+	}
+}
